921cd64eb242b588b727d6276eb7cd5796a12d0f
anas1590 <bcsf18m028@pucit.edu.pk>
2021-12-09 18:09:10 +0500

    first

---
+++ app/src/main/java/com/example/intenttoast/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.intenttoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
